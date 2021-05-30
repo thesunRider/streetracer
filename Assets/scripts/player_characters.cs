ab1f6d9f0d981d8961199323bc97d7833ab643bd
suryasaradhi <suryasaradhi3@gmail.com>
2021-05-31 03:32:47 +0530

    Car model
    Added Navs

--- Assets/scripts/player_characters.cs
+++ Assets/scripts/player_characters.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class player_characters : MonoBehaviour
 {
+
+    private Rigidbody car_rigid;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.
+        car_rigid = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 trans = new Vector3(0, 0, 1);
+       // car_rigid.transform.position = trans;
     }
 }
